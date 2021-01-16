18e0a9d824466173081d3ea7cc5a9e93d1373d06
Wojciech Mazur <wojtekemmm@gmail.com>
2021-01-16 17:29:15 +0100

    Shop - master page - Get cover URL

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Json;
 use App\Models\Record;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     // Master page
-    public function index() {
-
-        $records = Record::get();       // get all records
-        dd($records);                   // 'dump' records collection and 'die' (stop execution)
-        return view('shop.index');
+    public function index()
+    {
+        $records = Record::with('genre')->get();               // get all records
+        foreach ($records as $record) {
+            if(!$record->cover) {
+                $record->cover = 'https://coverartarchive.org/release/' . $record->title_mbid . '/front-250.jpg';
+            }
+        }
+        $result = compact('records');           // compact('records') is the same as ['records' => $records]
+        Json::dump($result);                    // open http://vinyl_shop.test/shop?json
+        return view('shop.index', $result);     // add $result as second parameter
     }
 
     // Detail page
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,26 +3,27 @@
 @section('title', 'Shop')
 
 @section('main')
-    <h1>Shop</h1>
 
-    // Single record card
-    <div class="row">
-    <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-        <div class="card">
-            <img class="card-img-top" src="/assets/vinyl.png" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Artist</h5>
-                <p class="card-text">Record title</p>
-                <a href="#!" class="btn btn-outline-info btn-sm btn-block">Show details</a>
-            </div>
-            <div class="card-footer d-flex justify-content-between">
-                <p>genre</p>
-                <p>
-                    € price
-                    <span class="ml-3 badge badge-success">stock</span>
-                </p>
+<h1>Shop</h1>
+<div class="row">
+    @foreach($records as $record)
+        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
+            <div class="card">
+                <img class="card-img-top" src="/assets/vinyl.png" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Artist</h5>
+                    <p class="card-text">Record title</p>
+                    <a href="#!" class="btn btn-outline-info btn-sm btn-block">Show details</a>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <p>genre</p>
+                    <p>
+                        € price
+                        <span class="ml-3 badge badge-success">stock</span>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
+    @endforeach
 </div>
 @endsection
