b6c70a9b59ee6cab197f5cdf66eaac406e16bb65
Wojciech Mazur <wojtekemmm@gmail.com>
2021-02-04 20:20:00 +0100

    email auth

--- app/Http/Controllers/User/HistoryController.php
+++ app/Http/Controllers/User/HistoryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Helpers\Json;
-use App\Models\{Order, Orderline};
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Helpers\Json;
+use App\Mail\OrderMail;
 use Illuminate\Http\Request;
 use App\Helpers\Facade\FacadeCart;
+use App\Models\{Order, Orderline};
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class HistoryController extends Controller
 {
@@ -30,7 +33,7 @@ class HistoryController extends Controller
         $order = new Order();
         $order->user_id = auth()->id();
         $order->total_price = FacadeCart::getTotalPrice();
-        $order->save();
+        //$order->save();
         // Retrieve the id of the last inserted order
         $order_id = $order->id;
         // Loop over the records array in the cart and save them to the orderlines table
@@ -42,13 +45,41 @@ class HistoryController extends Controller
             $orderline->cover = $record['cover'];
             $orderline->total_price = $record['price'];
             $orderline->quantity = $record['qty'];
-            $orderline->save();
+            // $orderline->save();
         }
         // Empty the cart
-        FacadeCart::empty();
+        //FacadeCart::empty();
         // Redirect to the history page
         $message = 'Thank you for your order.<br>The records will be delivered as soon as possible.';
+
+        // Send email confirmation
+        $this->confirmEmail();
+
         session()->flash('success', $message);
         return redirect('/user/history');
+    }
+
+    private function confirmEmail()
+    {
+        // construct the mail message
+        $message = '<p>Thank you for your order.<br>The records will be delivered as soon as possible.</p>';
+        $message .= '<ul>';
+        foreach (FacadeCart::getRecords() as $record) {
+            $message .= "<li>{$record['qty']} x {$record['artist']} - {$record['title']}</li>";
+        }
+        $message .= '</ul>';
+
+        // Get all admins
+        $admins = User::where('admin', true)->select('name', 'email')->get();
+
+        $email = new OrderMail($message);
+        Mail::to(Auth::user())
+            ->cc($admins)
+            ->send($email);
+    }
+
+    private function confirmWhatsApp()
+    {
+        //
     }
 }
